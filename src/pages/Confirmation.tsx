6a8e53e2325c67dc14e50523432bcc581d3ac89b
Caio Marques <mfr.caio@gmail.com>
2021-05-10 23:31:36 -0300

    Tela Plant Select e Enviroment Button

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
@@ -7,6 +8,12 @@ import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate("PlantSelect");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -19,7 +26,7 @@ export function Confirmation() {
         </Text>
 
         <View style={styles.footer}>
-          <Button title={"Confirmar"} />
+          <Button title={"Confirmar"} onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
